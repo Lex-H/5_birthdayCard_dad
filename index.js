89f43daabe60d6cf1fd95f33f2d61681cfdefb76
Lex-H <ckfly4014737@gmail.com>
2024-08-07 18:51:23 +0800

    嵌入FB留言

--- index.js
+++ index.js
@@ -32,3 +32,9 @@ for (i=1;i<=36;i++) {
     countI =+1;
 }
 document.querySelector(".picContainer").innerHTML = picTemple;
+
+// 修改FB貼文寬度，桌面版才需要，手機版寬度會貼合容器
+// let screenWidth = screen.width;
+// let fbPostWidth = screen.width*0.73;
+// fbPostWidth = fbPostWidth.toString();
+// document.getElementById("fbPost").setAttribute("data-width", fbPostWidth);
